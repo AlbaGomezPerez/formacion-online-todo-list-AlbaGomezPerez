6a9818ea24d7f5453037bed86cc140043d8f4d17
Alba Gómez Pérez <albagope@gmail.com>
2019-11-21 11:28:46 +0100

    add date

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const dateText = document.querySelector('.date');
+
+let date = new Date();
+date.getDate()
+{
+    dateText.innerHTML= date;
+    console.log('soy la fecha' + date);
+}
+
